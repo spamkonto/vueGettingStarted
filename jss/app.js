999dc85f45ee9abadd157606068e9a82656889ff
Spam Konto <spam.konto@onet.pl>
2018-08-29 09:05:15 +0200

    End of first part

--- jss/app.js
+++ jss/app.js
@@ -42,3 +42,26 @@ var app5 = new Vue({
 		}
 	}
 })
+
+var app6 = new Vue({
+	el: '#app-6',
+	data: {
+		message: 'Hello Vue!'
+	}
+})
+
+Vue.component('todo-item', {
+	props: ['todo'],
+  	template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+	el: '#app-7',
+	data: {
+		groceryList: [
+			{ id: 0, text: 'Vegetables'},
+			{ id: 1, text: 'Cheese'},
+			{ id: 2, text: 'Whatever else humans are supposed to eat'}
+		]
+	}
+})
